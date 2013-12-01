6fa9dc3e6926b7a977828d77db6d5b8fdd1e68f3
redmaner <jakevdputten@gmail.com>
2013-12-01 17:45:23 +0100

    micore_setup.sh: add version number cfg and bugfixes/improvements

--- micore_setup.sh
+++ micore_setup.sh
@@ -9,16 +9,16 @@ check_cfg () {
 if [ -e $MICORE_CFG ]; then
      echo -en "Micore build enivonment is already configured\nThe following configurartion has been found:\n\n`cat $MICORE_CFG`\n\nDo you want to reconfigure the build environement (y,n)?"; read check_cfg_option
      case "$check_cfg_option" in
-         y) rm $MICORE_CFG; make_cfg_toolchain;;
-         n) clear; exit;;
+         y) rm $MICORE_CFG; make_cfg_toolchain; make_cfg_toolchain_path; make_cfg_version; make_micore_environment;;
+         n) clear;;
      esac
 else 
-    make_cfg_toolchain;
+    make_cfg_toolchain; make_cfg_toolchain_path; make_cfg_version; make_micore_environment
 fi
 }
 
 make_cfg_toolchain () {
-echo -en "Which toolchain do you use (1,2)?\n 1 - GCC (default)\n 2 - Linaro\n "; read toolchain_option
+echo -en "\nWhich toolchain do you use (1,2)?\n 1 - GCC (default)\n 2 - Linaro\n "; read toolchain_option
 case "$toolchain_option" in 
     1) TOOLCHAIN=gcc;;
     2) TOOLCHAIN=linaro;;
@@ -27,22 +27,27 @@ esac
 }
 
 make_cfg_toolchain_path () {
-echo -en "Please enter the path to your $TOOLCHAIN toolchain\ne.g. /opt/toolchains/arm-eabi-4.7/bin/arm-eabi-: "; read toolchain_path_option
-if [ ! -e "$toolchain_path_option"gcc ]; then
-     echo "The path: $toolchain_path_option\nDoes not exsist\nRe-enter your path"; make_cfg_toolchain_path
+echo -en "\nEnter the path to your $TOOLCHAIN toolchain\ne.g. /opt/toolchains/arm-eabi-4.7/bin/arm-eabi-: "; read toolchain_path_option
+if [ ! -e "$toolchain_path_option"g++ ]; then
+     echo -en "\nThe path: $toolchain_path_option\nDoes not exsist, re-enter your path"; make_cfg_toolchain_path
 else
      CROSS_COMPILE=$toolchain_path_option
 fi
-make_micore_invironment
 }
+
+make_cfg_version () {
+echo -en "\nEnter a version for MiCore build: "; read version_option
+echo -en "Version can be updated with: ./make_micore.sh --version [version]"
+}
+
 make_micore_environment () {
 echo "TOOLCHAIN=$TOOLCHAIN" > $MICORE_CFG
-echo "CROSS=$CROSS_COMPILE" >> $MICORE_CFG
+echo "CROSS_COMPILE=$CROSS_COMPILE" >> $MICORE_CFG
 
 chmod 755 micore_tools/scripts/*
 chmod 755 micore_tools/bootimage_tools/*
 ln -sf micore_tools/scripts/$MICORE_MAIN $MICORE_MAIN
+echo -en "\n\nConfiguration of MiCore build environment succesfully complete!\n"; sleep 2
 }
 
-clear; check_cfg; exit
-
+clear; check_cfg
