3acc7d1686da8f48cee58aacac7d881c7eb7b166
redmaner <jakevdputten@gmail.com>
2013-12-18 19:56:58 +0100

    aries: initramfs: Upsteam changes for MiCore R3v1

--- aries/initramfs/sbin/micore-init.sh
+++ aries/initramfs/sbin/micore-init.sh
@@ -3,6 +3,11 @@
 export PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 
 mount -o remount,rw /system
+if [ ! -e /system/etc/micore.cfg ]; then
+     cp /sbin/micore.cfg /system/etc/micore.cfg
+fi
+source /system/etc/micore.cfg
+
 # modules permissions
 if [ ! -e /system/lib/modules/wlan.ko ]; then
      chmod -R 755 /system/lib/modules
@@ -17,9 +22,6 @@ if [ -e /system/bin/mpdecision ]; then
      mv /system/bin/mpdecision /system/bin/mpdecision.bak
 fi
 mount -o remount,ro /system
-
-# Limit frequency driver
-insmod /system/lib/modules/cpufreq_limit.ko
 
 # CPU freq optimizations
 MAX_FREQ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
@@ -39,10 +41,27 @@ echo "10000" > /sys/module/msm_kgsl_core/parameters/simple_ramp_threshold
 # VM optimizations
 echo "4096" > /proc/sys/vm/min_free_kbytes
 echo "0" > /proc/sys/vm/swappiness
+sync; echo "3" > /proc/sys/vm/drop_caches
+sync; echo "1" > /proc/sys/vm/drop_caches
+
+# ZRAM
+if [ "$ZRAM" = "y" ]; then
+     insmod /system/lib/modules/zram.ko
+     if [ -f /sys/devices/virtual/block/zram0/disksize ]; then
+          echo $(($ZRAM_MB * 1024 * 1024)) > /sys/devices/virtual/block/zram0/disksize
+          mkswap /dev/block/zram0
+          swapon /dev/block/zram0
+          echo "100" > /proc/sys/vm/swappiness
+          echo "100" > /proc/sys/vm/vfs_cache_pressure
+     fi
+fi
 
 # fstrim
-/sbin/fstrim -v /cache
-/sbin/fstrim -v /data
+if [ "$TRIM" = "y" ]; then
+     /sbin/fstrim -v /cache
+     /sbin/fstrim -v /data
+     /sbin/fstrim -v /system
+fi
 
 # sysctl 
 if [ -e /system/etc/sysctl.conf ]; then
