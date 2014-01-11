e845a2f3c32d371e11d3c1a5a15fa55f01716c24
redmaner <jakevdputten@gmail.com>
2014-01-12 00:38:57 +0100

    aries: micore 008m
    Set westwood as default tcp
    Support for intelli thermal
    Move all install files /res folder

--- aries/initramfs/sbin/micore-init.sh
+++ aries/initramfs/sbin/micore-init.sh
@@ -5,17 +5,22 @@ export PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 mount -o remount,rw /system
 # Modules
 if [ ! -d /system/lib/modules ]; then
-     cp -r /lib/modules /system/lib/modules
-     chmod -R 755 /system/lib/modules
-     chmod -R 755 /system/lib/modules/prima
-     chmod 644 /system/lib/modules/*.ko
-     chmod 644 /system/lib/modules/prima/prima_wlan.ko
-     ln -s /system/lib/modules/prima/prima_wlan.ko /system/lib/modules/wlan.ko
+     	cp -r /res/modules /system/lib/modules
+     	chmod -R 755 /system/lib/modules
+     	chmod -R 755 /system/lib/modules/prima
+     	chmod 644 /system/lib/modules/*.ko
+     	chmod 644 /system/lib/modules/prima/prima_wlan.ko
+     	ln -s /system/lib/modules/prima/prima_wlan.ko /system/lib/modules/wlan.ko
+fi
+
+# Thermald.conf
+if [ ! -e /system/etc/thermald.conf ]; then
+	cp /res/thermald.conf /system/etc/thermald.conf
 fi
 
 # Disable MPDecision
 if [ -e /system/bin/mpdecision ]; then
-     mv /system/bin/mpdecision /system/bin/mpdecision.bak
+     	mv /system/bin/mpdecision /system/bin/mpdecision.bak
 fi
 mount -o remount,ro /system
 
@@ -31,35 +36,39 @@ done
 /sbin/fstrim -v /data
 /sbin/fstrim -v /system
 
+# Intelli-thermal
+echo "Y" > /sys/modules/msm_thermal/parameters/enabled
+
 # Intellidemand optimizations
 MAX_FREQ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
-echo "384000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo "95" > /sys/devices/system/cpu/cpufreq/intellidemand/up_threshold
 echo "85" > /sys/devices/system/cpu/cpufreq/intellidemand/up_threshold_any_cpu_load
 echo "75" > /sys/devices/system/cpu/cpufreq/intellidemand/up_threshold_multi_core
-echo "1134000, 1134000, 1134000, 1134000" > /sys/devices/system/cpu/cpufreq/intellidemand/two_phase_freq
+echo "1134000" > /sys/devices/system/cpu/cpufreq/intellidemand/two_phase_freq
 echo "$MAX_FREQ" > /sys/devices/system/cpu/cpufreq/intellidemand/optimal_freq
 echo "810000" > /sys/devices/system/cpu/cpufreq/intellidemand/sync_freq
-echo "1350000, 1350000, 1350000, 1350000" > /sys/devices/system/cpu/cpufreq/intellidemand/input_event_min_freq
+echo "1350000" > /sys/devices/system/cpu/cpufreq/intellidemand/boostfreq
 
-# Simple GPU governor
-echo "simple" > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/pwrscale/trustzone/governor
-echo "10000" > /sys/module/msm_kgsl_core/parameters/simple_ramp_threshold
-
-# VM optimizations
+# Sysctl
 echo "4096" > /proc/sys/vm/min_free_kbytes
 echo "0" > /proc/sys/vm/swappiness
+echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
 sync; echo "3" > /proc/sys/vm/drop_caches
 sync; echo "1" > /proc/sys/vm/drop_caches
 
+# read a head buffer 1mb
+echo "1024" > /sys/block/mmcblk0/queue/read_ahead_kb
+
 # sysctl 
 if [ -e /system/etc/sysctl.conf ]; then
-     syctl -p /system/etc/sysctl.conf
+     	syctl -p /system/etc/sysctl.conf
 fi
 
 # init.d
 if [ -d /system/etc/init.d ]; then
-    chmod -R 755 /system/etc/init.d
-    chmod 755 /system/etc/init.d/*
-    /system/bin/logwrapper /sbin/run-parts /system/etc/init.d 
+    	chmod -R 755 /system/etc/init.d
+    	chmod 755 /system/etc/init.d/*
+    	/system/bin/logwrapper /sbin/run-parts /system/etc/init.d 
+else
+	mkdir /system/etc/init.d
 fi
--- scripts/make_boot_aries.sh
+++ scripts/make_boot_aries.sh
@@ -2,7 +2,7 @@
 source micore_tools/micore.cfg
 DEVICE=$1
 INITRAMFS_TMP=/tmp/initramfs-source
-INITRAMFS_SOURCE=micore_tools/$DEVICE/initramfs
+INITRAMFS_SOURCE=micore_tools/aries/initramfs
 
 #remove previous initramfs files
 rm -rf $INITRAMFS_TMP
@@ -19,7 +19,7 @@ find $INITRAMFS_TMP -name EMPTY_DIRECTORY -exec rm -rf {} \;
 rm -rf $INITRAMFS_TMP/tmp/*
 
 #copy modules into system folder
-MICORE_MODULES=$INITRAMFS_TMP/lib/modules
+MICORE_MODULES=$INITRAMFS_TMP/res/modules
 rm -rf $MICORE_MODULES
 mkdir -p $MICORE_MODULES/prima
 find -name '*.ko' -exec cp -av {} $MICORE_MODULES \;
@@ -41,22 +41,23 @@ micore_tools/bootimage_tools/mkbootfs /tmp/initramfs-source | gzip > micore_tool
 micore_tools/bootimage_tools/mkbootimg --kernel arch/arm/boot/zImage --ramdisk micore_tools/initrd.img --cmdline "console=null androidboot.hardware=qcom ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o micore_tools/aries/zip_template/boot.img
 
 # add updater-script
-cat > micore_tools/$DEVICE/zip_template/META-INF/com/google/android/updater-script << EOF
-assert(getprop("ro.product.device") == "$DEVICE" ||
-       getprop("ro.build.product") == "$DEVICE");
+cat > micore_tools/aries/zip_template/META-INF/com/google/android/updater-script << EOF
+assert(getprop("ro.product.device") == "aries" ||
+       getprop("ro.build.product") == "aries");
 show_progress(0.500000, 0);
 ui_print(" ");
 ui_print("MiCore Kernel $VERSION");
 ui_print("For Xiaomi MI-2/S (aries)");
 mount("ext4", "EMMC", "/dev/block/platform/msm_sdcc.1/by-name/system", "/system");
 delete_recursive("/system/lib/modules");
+delete("/system/etc/thermald.conf");
 package_extract_file("boot.img", "/dev/block/platform/msm_sdcc.1/by-name/boot");
 unmount("/system");
 EOF
 
 # make zip archive
-rm -f micore_tools/$DEVICE/zip_template/kernel.zip
-cd micore_tools/$DEVICE/zip_template
+rm -f micore_tools/aries/zip_template/kernel.zip
+cd micore_tools/aries/zip_template
 zip -r kernel.zip *
 cd ../../..
-java -jar micore_tools/signapk/signapk.jar micore_tools/signapk/testkey.x509.pem micore_tools/signapk/testkey.pk8 micore_tools/$DEVICE/zip_template/kernel.zip micore_tools/out/MiCore_"$VERSION"_"$DEVICE".zip
+java -jar micore_tools/signapk/signapk.jar micore_tools/signapk/testkey.x509.pem micore_tools/signapk/testkey.pk8 micore_tools/aries/zip_template/kernel.zip micore_tools/out/MiCore_"$VERSION"_"aries".zip
